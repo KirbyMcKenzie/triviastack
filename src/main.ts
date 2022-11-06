684d9f42224a27df7ad33b42625cdc77fe5eca98
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2022-11-06 17:36:37 +1100

    Add supabase client with ability to read and write questions

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
+import axios from 'axios';
 import { App } from '@slack/bolt';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 import { decodeEscapedHTML, titleCase } from './helpers/string';
 dotenv.config();
-import axios from 'axios';
+
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://dlxythnybkfecdobxkkg.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -11,87 +16,85 @@ const app = new App({
 });
 
 app.command('/trivia', async ({ ack, say }) => {
-  try {
-    await say(
-      '|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n',
-    );
-    await ack();
-    axios
-      .get('https://opentdb.com/api.php?amount=10')
-      .then(async (res) => {
-        const questions = res.data.results;
+  await ack();
+  await say('Quick quiz coming up!');
 
-        questions.forEach(async (question) => {
-          console.log(question, 'question');
-          const decodedQuestion = decodeEscapedHTML(question.question);
+  axios.get('https://opentdb.com/api.php?amount=10').then(async (res) => {
+    const { error } = await supabase
+      .from('quizzes')
+      .insert({ questions: res.data.results, is_active: true });
 
-          console.log(`📣 [Trivia] - ${decodedQuestion}`);
+    !!error && console.log(`SUPABASE ERROR: ${error}`);
 
-          const answers = question.incorrect_answers.map((answer) => ({
-            type: 'button',
-            text: {
-              type: 'plain_text',
-              text: answer,
-              emoji: true,
+    const firstQuestion = res.data.results[0];
+
+    // const answers = firstQuestion.incorrect_answers.map((answer) => ({
+    //   type: 'button',
+    //   text: {
+    //     type: 'plain_text',
+    //     text: answer,
+    //     emoji: true,
+    //   },
+    //   action_id: 'button_click',
+    //   value: answer,
+    // }));
+
+    await say({
+      blocks: [
+        {
+          type: 'divider',
+        },
+
+        {
+          type: 'section',
+          text: {
+            type: 'plain_text',
+            text: decodeEscapedHTML(firstQuestion.question),
+            emoji: true,
+          },
+        },
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: `*Category:* ${
+                firstQuestion.category
+              }  *·*  *Difficulty:* ${titleCase(firstQuestion.difficulty)}`,
             },
-            value: 'click_me_123',
-          }));
+          ],
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: firstQuestion.correct_answer,
+                emoji: true,
+              },
+              action_id: 'button_click',
+              value: firstQuestion.correct_answer,
+            },
+            // ...answers,
+          ],
+        },
+      ],
+    });
+  });
+});
 
-          await say({
-            blocks: [
-              {
-                type: 'divider',
-              },
-
-              {
-                type: 'section',
-                text: {
-                  type: 'plain_text',
-                  text: decodedQuestion,
-                  emoji: true,
-                },
-              },
-              {
-                type: 'context',
-                elements: [
-                  {
-                    type: 'mrkdwn',
-                    text: `*Category:* ${
-                      question.category
-                    }  *·*  *Difficulty:* ${titleCase(question.difficulty)}`,
-                  },
-                ],
-              },
-              {
-                type: 'actions',
-                elements: [
-                  {
-                    type: 'button',
-                    text: {
-                      type: 'plain_text',
-                      text: question.correct_answer,
-                      emoji: true,
-                    },
-                    value: 'click_me_123',
-                  },
-                  ...answers,
-                ],
-              },
-            ],
-          });
-        });
-      })
-      .catch((err) => {
-        console.log('Error: ', err.message);
-      });
-  } catch (error) {
-    console.log(error, 'err');
-    // console.error(error);
-  }
+app.action('button_click', async ({ body, ack, say, payload }) => {
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
+  console.log(body, 'body');
+  console.log('-=-=-=-=-=-=-=-=-=-=-=-=-=');
+  console.log(payload, 'payload');
 });
 
 app.message('yo', async ({ message, say }) => {
-  // say() sends a message to the channel where the event was triggered
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //@ts-ignore
   await say(`Hey there <@${message.user}>!`);
 });
