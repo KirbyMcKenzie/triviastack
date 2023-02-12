2d5445f357d6d7043a7c2f4826fd748500c5bf79
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2023-02-12 11:22:48 +1100

    Temp reenable yeet on prod

--- listeners/messages/index.ts
+++ listeners/messages/index.ts
@@ -7,7 +7,8 @@ import handleYoza from "./handleMessageYoza";
 const isDev = process.env.NODE_ENV === "development";
 
 const registerMessages = (app: App) => {
-  isDev && app.message("yeet", handleYeet);
+  // isDev && app.message("yeet", handleYeet);
+  app.message("yeet", handleYeet);
   app.message("yoza", handleYoza);
   app.message(/bloody oath/, handleBloodyOath);
 };
