d505ccc3cba0938edc49b4b2e75320fb7eed06f7
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2023-03-04 20:43:50 +1100

    Can now create shcedules from modal

--- listeners/views/handleSubmitCreateSchedule.ts
+++ listeners/views/handleSubmitCreateSchedule.ts
@@ -3,16 +3,17 @@ import {
   SlackViewAction,
   SlackViewMiddlewareArgs,
 } from "@slack/bolt/dist/types";
+import { createSchedule } from "services/scheduleService";
 
 const handleSubmitCreateSchedule = async ({
   ack,
   body,
   view,
-  client,
 }: SlackViewMiddlewareArgs<SlackViewAction> & AllMiddlewareArgs) => {
   await ack();
 
   const userId = body.user.id;
+  const teamId = body.team?.id;
   const { input_time, input_timezone } = view.state.values;
 
   const {
@@ -23,9 +24,13 @@ const handleSubmitCreateSchedule = async ({
     select_timezone: { selected_option: selected_timezone },
   } = input_timezone;
 
-  console.log(userId, "userId");
-  console.log(selected_time, "input_time::selected_time");
-  console.log(selected_timezone?.value, "input_timezone::value");
+  await createSchedule({
+    createdBy: userId,
+    time: selected_time as string, // TODO: add timezone?
+    timezone: selected_timezone?.value as string,
+    channelId: "42069",
+    teamId: teamId as string,
+  });
 
   // await client.chat.postMessage({
   //   channel: channelId,
---
+++ services/scheduleService.ts
@@ -0,0 +1,30 @@
+import { SlackUser } from "types/user";
+import { supabase } from "../clients/supabase";
+
+export interface CreateSchedule {
+  teamId: string;
+  channelId: string;
+  createdBy: string;
+  time: string;
+  timezone: string;
+}
+
+export const createSchedule = async ({
+  teamId,
+  channelId,
+  createdBy,
+  time,
+  timezone,
+}: CreateSchedule): Promise<void> => {
+  const { data, error } = await supabase
+    .from("schedules")
+    .insert({
+      team_id: teamId,
+      channel_id: channelId,
+      created_by: createdBy,
+      time,
+      timezone,
+    })
+    .select();
+  error && console.log(error, "[SQL ERROR]");
+};
