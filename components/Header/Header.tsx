757f2bbbf143d2c39ed684efe6d066d64fd24a88
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2023-03-12 16:35:04 +1100

    Fix next link

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
 function Header() {
@@ -24,35 +25,37 @@ function Header() {
           {/* Site branding */}
           <div className="flex-shrink-0 mr-4">
             {/* Logo */}
-            <a href="/" className="block" aria-label="Cruip">
-              <svg
-                className="w-8 h-8"
-                viewBox="0 0 32 32"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <defs>
-                  <radialGradient
-                    cx="21.152%"
-                    cy="86.063%"
-                    fx="21.152%"
-                    fy="86.063%"
-                    r="79.941%"
-                    id="header-logo"
-                  >
-                    <stop stopColor="#4FD1C5" offset="0%" />
-                    <stop stopColor="#81E6D9" offset="25.871%" />
-                    <stop stopColor="#338CF5" offset="100%" />
-                  </radialGradient>
-                </defs>
-                <rect
-                  width="32"
-                  height="32"
-                  rx="16"
-                  fill="url(#header-logo)"
-                  fillRule="nonzero"
-                />
-              </svg>
-            </a>
+            <Link href="/">
+              <a className="block" aria-label="Cruip">
+                <svg
+                  className="w-8 h-8"
+                  viewBox="0 0 32 32"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <defs>
+                    <radialGradient
+                      cx="21.152%"
+                      cy="86.063%"
+                      fx="21.152%"
+                      fy="86.063%"
+                      r="79.941%"
+                      id="header-logo"
+                    >
+                      <stop stopColor="#4FD1C5" offset="0%" />
+                      <stop stopColor="#81E6D9" offset="25.871%" />
+                      <stop stopColor="#338CF5" offset="100%" />
+                    </radialGradient>
+                  </defs>
+                  <rect
+                    width="32"
+                    height="32"
+                    rx="16"
+                    fill="url(#header-logo)"
+                    fillRule="nonzero"
+                  />
+                </svg>
+              </a>
+            </Link>
           </div>
           <span className="font-bold">triviabot.io</span>
           <nav className="flex flex-grow">
