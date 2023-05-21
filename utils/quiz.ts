a03f558a578274c6827fc476a3d8dbd102d73b08
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2023-05-21 18:32:40 +1000

    Add more variety in game over responses

--- utils/quiz.ts
+++ utils/quiz.ts
@@ -7,11 +7,15 @@ export const getGameOverResponse = (score: number, max: number): string => {
     case percent >= 100:
       return getRandomCollectionItem(SCORE_RESPONSES["PERFECT"]);
     case percent < 100 && percent >= 80:
-      return getRandomCollectionItem(SCORE_RESPONSES["GOOD"]);
+      return getRandomCollectionItem(SCORE_RESPONSES["GREAT"]);
     case percent < 80 && percent >= 60:
+      return getRandomCollectionItem(SCORE_RESPONSES["GOOD"]);
+    case percent < 60 && percent >= 50:
       return getRandomCollectionItem(SCORE_RESPONSES["OKAY"]);
-    default:
+    case percent < 50 && percent >= 30:
       return getRandomCollectionItem(SCORE_RESPONSES["POOR"]);
+    default:
+      return getRandomCollectionItem(SCORE_RESPONSES["AWFUL"]);
   }
 };
 
@@ -33,102 +37,87 @@ export const getGameOverEmoji = (score: number, max: number): string => {
       return getRandomCollectionItem(["💩", "💀", "🫣", "😵", "🤡"]);
   }
 };
-
 export const PERFECT_SCORE_RESPONSES = [
-  "Flawless victory!",
-  "You're a genius!",
-  "Unbelievable performance!",
-  "You aced it!",
-  "Mind-blowing work!",
-  "Absolutely outstanding!",
-  "Perfection personified!",
-  "You're on fire!",
-  "Stellar job, superstar!",
-  "Masterful execution!",
-  "Excellence at its finest!",
-  "You're unstoppable!",
-  "Astounding display of skill!",
-  "A perfect masterpiece!",
-  "Unparalleled achievement!",
-  "Pure brilliance!",
-  "Unreal performance!",
-  "You're a legend!",
-  "Unmatched success!",
-  "Incredible triumph!",
+  "Great job! Perfect score!",
+  "Incredible! Couldn't be better!",
+  "Flawless! You're a pro!",
+  "Wow! You nailed it!",
+  "Amazing work! You're unstoppable!",
+  "Outstanding! Perfect score!",
+  "Impressive! You're a natural!",
+  "Brilliant! Flawless victory!",
+  "Excellent! You aced it!",
+  "Perfection! Well done!",
+];
+
+export const GREAT_SCORE_RESPONSES = [
+  "Nice work! You're mastering this!",
+  "Fantastic! You're on fire!",
+  "Impressive! Keep it up!",
+  "Well done! Great score!",
+  "Terrific effort! Keep going!",
+  "Bravo! You're doing amazing!",
+  "Awesome job! Great score!",
+  "Superb performance! Keep it up!",
+  "You're crushing it! Great score!",
+  "Fantastic work! You're unstoppable!",
 ];
 
 export const GOOD_SCORE_RESPONSES = [
-  "You're rocking it!",
-  "Way to go!",
-  "Fantastic effort!",
-  "Bravo!",
-  "You're killing it!",
-  "Impressive skills!",
-  "Terrific job!",
-  "You're a star!",
-  "Keep up the amazing work!",
-  "Superb performance!",
-  "Outstanding display!",
-  "You're nailing it!",
-  "Excellent job, champ!",
-  "Great work, maestro!",
-  "You're crushing it!",
-  "Exceptional talent!",
-  "Magnificent effort!",
-  "You've got what it takes!",
-  "Top-notch performance!",
-  "Remarkable achievement!",
+  "Good job! Excellent progress!",
+  "Well done! You're getting the hang of it!",
+  "Nice work! Getting better!",
+  "Keep it up! Good score!",
+  "Solid effort! You're improving!",
+  "Thumbs up! Good job!",
+  "You're doing great! Good score!",
+  "Well played! Good job!",
+  "You're on the right track! Good score!",
+  "Impressive! You're making progress!",
 ];
 
 export const OKAY_SCORE_RESPONSES = [
-  "Not too shabby!",
-  "Decent work!",
-  "Progress is evident!",
-  "You're making strides!",
-  "You're on the right path!",
-  "Promising performance!",
-  "Keep pushing forward!",
-  "You're improving!",
-  "Showing potential!",
-  "Good effort!",
-  "Room for growth, but not bad!",
-  "You're getting there!",
-  "Getting better every time!",
-  "Noteworthy attempt!",
-  "Keep it up!",
-  "You're on the rise!",
-  "Moving in the right direction!",
-  "You're making headway!",
-  "Growing skills!",
-  "Nice progress!",
+  "Okay, not bad! You're getting there!",
+  "Decent effort! Keep practicing!",
+  "Not too shabby! Keep it up!",
+  "Nice try! Keep going!",
+  "You're making progress! Okay score!",
+  "Not bad at all! Keep it up!",
+  "You're on the right path! Okay score!",
+  "Good effort! Okay score!",
+  "Well done! You're improving! Okay score!",
+  "You're getting better! Okay score!",
 ];
 
 export const POOR_SCORE_RESPONSES = [
-  "Step up your game!",
-  "You can do better than this!",
-  "Don't give up!",
-  "Disappointing effort.",
-  "You're capable of more!",
-  "Try harder next time!",
-  "Learn from this experience!",
-  "You're stronger than this!",
-  "Keep practicing!",
-  "Persevere and improve!",
-  "Don't lose heart!",
-  "Chin up and try again!",
-  "Failure is a stepping stone!",
-  "Don't let setbacks define you!",
-  "Believe in your abilities!",
-  "Don't settle for mediocrity!",
-  "Challenge yourself to grow!",
-  "Strive for greatness!",
-  "Turn this setback into a comeback!",
-  "Dust yourself off and try again!",
+  "Hmm, a little off this time.",
+  "Keep trying! You'll improve!",
+  "Don't worry! You'll get better!",
+  "You can do it! Poor score!",
+  "Mistakes happen! Keep going!",
+  "Don't give up! Poor score!",
+  "Keep practicing! Poor score!",
+  "It's okay! You'll bounce back!",
+  "Stay positive! Poor score!",
+  "You're learning! Poor score!",
+];
+
+export const AWFUL_SCORE_RESPONSES = [
+  "Oh damn, didn't even get one correct",
+  "Thats embarrassing",
+  "oof",
+  "Yikes",
+  "What a train wreck!",
+  "Did you forget how to play?",
+  "Better luck next time!",
+  "(╯°□°)╯︵ ┻━┻",
 ];
 
 export const SCORE_RESPONSES = {
   PERFECT: PERFECT_SCORE_RESPONSES,
+  GREAT: GREAT_SCORE_RESPONSES,
   GOOD: GOOD_SCORE_RESPONSES,
   OKAY: OKAY_SCORE_RESPONSES,
   POOR: POOR_SCORE_RESPONSES,
+  AWFUL: AWFUL_SCORE_RESPONSES,
 };
