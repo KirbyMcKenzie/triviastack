2254c786cc08b26287861ae31b7925741260ccf1
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2023-06-24 16:30:30 +1000

    Purge old env var flags, tidy up handleQuickQuiz

--- listeners/commands/handleQuickQuiz.ts
+++ listeners/commands/handleQuickQuiz.ts
@@ -3,14 +3,7 @@ import {
   SlackCommandMiddlewareArgs,
 } from "@slack/bolt/dist/types";
 import { createNewJob } from "services/jobService";
-import { createNewQuiz, fetchQuizQuestions } from "services/quizService";
-import {
-  buildErrorMaxQuestionsExceeded,
-  buildQuestionBlock,
-} from "utils/blocks";
-import { getRandomCollectionItem } from "utils/random";
-
-const useNewQuizFlow = process.env.USE_NEW_QUIZ_FLOW === "enabled";
+import { buildErrorMaxQuestionsExceeded } from "utils/blocks";
 
 const DEFAULT_NUM_QUESTIONS = 10;
 const MAX_QUESTIONS = 30;
@@ -26,13 +19,13 @@ const handleQuickQuiz = async ({
   await ack();
   logger.info(`[COMMAND] Quick quiz triggered by ${payload.user_id}`);
 
-  const isDirectMessage = payload.channel_name === "directmessage";
-  let ts;
-
   const numberOfQuestions = parseInt(payload.text) || DEFAULT_NUM_QUESTIONS;
   if (numberOfQuestions > MAX_QUESTIONS) {
     return await respond(buildErrorMaxQuestionsExceeded(MAX_QUESTIONS));
   }
+
+  const isDirectMessage = payload.channel_name === "directmessage";
+  let ts;
 
   if (!isDirectMessage) {
     const loadingMessage = await say({
@@ -50,40 +43,18 @@ const handleQuickQuiz = async ({
     ts = loadingMessage.ts;
   }
 
-  if (useNewQuizFlow || isDirectMessage) {
-    logger.info(`[COMMAND] Creating new quiz via job`);
-    await createNewJob({
-      createdBy: payload.user_id,
-      teamId: payload.team_id,
-      type: "CREATE_QUIZ",
-      payload: {
-        channel_name: payload.channel_name,
-        channel_id: payload.channel_id,
-        number_of_questions: numberOfQuestions,
-        ts: ts,
-      },
-    });
-  } else {
-    logger.info(`[COMMAND] Creating new quiz via old workflow`);
-
-    const questions = await fetchQuizQuestions({ numberOfQuestions });
-
-    const questionBlock = buildQuestionBlock({
-      question: questions[0],
-      currentQuestion: 1,
-      totalQuestions: questions.length,
-      userId: payload.user_id,
-      isSuperQuiz: numberOfQuestions === MAX_QUESTIONS,
-    });
-
-    await client.chat.update({
-      channel: payload.channel_id,
-      blocks: questionBlock.blocks,
-      ts: ts as string,
-    });
-
-    await createNewQuiz(questions, payload.channel_id);
-  }
+  logger.info(`[COMMAND] Creating new quiz via job`);
+  await createNewJob({
+    createdBy: payload.user_id,
+    teamId: payload.team_id,
+    type: "CREATE_QUIZ",
+    payload: {
+      channel_name: payload.channel_name,
+      channel_id: payload.channel_id,
+      number_of_questions: numberOfQuestions,
+      ts: ts,
+    },
+  });
 };
 
 export default handleQuickQuiz;
