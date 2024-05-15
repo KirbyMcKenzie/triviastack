36447b7f0abcfd251b40f04a885b920efb13f9a8
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-05-15 16:45:21 -0400

    Update favicon to new logo

--- components/ButtonAddToSlack/ButtonAddToSlack.tsx
+++ components/ButtonAddToSlack/ButtonAddToSlack.tsx
@@ -10,7 +10,7 @@ const ButtonAddToSlack: FC<Props> = ({ height = 54, width = 184 }) => {
   return (
     <a
       href="https://triviastack.fly.dev/slack/install"
-      className="inline-flex items-center justify-center no-underline font-semibold px-4  rounded-full"
+      className="inline-flex items-center justify-center no-underline font-semibold px-4 rounded-full hover:shadow-lg transition-all duration-300 ease-in-out"
       style={{ height, width, backgroundColor: "#2758E7", color: "white" }}
     >
       <svg
@@ -26,14 +26,17 @@ const ButtonAddToSlack: FC<Props> = ({ height = 54, width = 184 }) => {
         <path
           d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
           fill="#fff"
+          // fill="#36c5f0"
         ></path>
         <path
           d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
           fill="#fff"
+          // fill="#2eb67d"
         ></path>
         <path
           d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
           fill="#fff"
+          // fill="#ecb22e"
         ></path>
       </svg>
       Add to Slack
