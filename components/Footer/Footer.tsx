da03ff336fcb46fa1aefd81b3d1693f998876a79
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-05-24 13:16:30 -0400

    Update Footer to include links to support and privacy pages

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,15 +6,17 @@ function Footer() {
     <footer>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
-          <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
-            <li>
-              <Link
-                href="https://twitter.com/triviastack"
-                aria-label="Twitter"
-              >
+          <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0 gap-x-4">
+            <li className="hover:text-blue-600">
+              <Link href="/support">Support</Link>
+            </li>
+            <li className="hover:text-blue-600">
+              <Link href="/privacy.html">Privacy Policy</Link>
+            </li>
+            {/* <Link href="https://twitter.com/triviastack" aria-label="Twitter">
                 <a
                   target="blank"
-                  className="flex justify-center items-center text-white hover:text-blue-400 rounded-full transition duration-150 ease-in-out"
+                  className="flex justify-center items-center hover:text-blue-400 rounded-full transition duration-150 ease-in-out"
                 >
                   <svg
                     className="w-8 h-8 fill-current"
@@ -24,17 +26,15 @@ function Footer() {
                     <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
                   </svg>
                 </a>
-              </Link>
-            </li>
+              </Link> */}
           </ul>
 
-          {/* Copyrights note */}
-          <div className="text-sm mr-4">
-            Made by{" "}
+          <div className="mr-4">
+            Created by{" "}
             <a
-              className="hover:underline font-medium"
+              className="hover:underline font-medium hover:text-blue-600"
               target="blank"
-              href="https://kirby.co.nz?ref=triviastack"
+              href="https://twitter.com/kirbymckenzie_"
             >
               Kirby McKenzie
             </a>
