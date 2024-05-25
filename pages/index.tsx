39601255cc453809dcd2e24c1742c69a8a71933e
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-05-25 16:04:06 -0400

    Add seo thumbnail

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,10 +20,7 @@ const Home: NextPage = () => {
           property="og:description"
           content="Keep your team engaged with Trivia for Slack."
         />
-        {/* <meta
-          property="og:image"
-          content="https://t3.ftcdn.net/jpg/00/92/53/56/360_F_92535664_IvFsQeHjBzfE6sD4VHdO8u5OHUSc6yHF.jpg"
-        /> */}
+        <meta property="og:image" content="/og-thumbnail.png" />
       </Head>
       <div className="flex flex-col min-h-screen">
         <Header />
