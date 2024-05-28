0aee09da2b3329508e5e8931a1050562f64be282
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-05-28 14:24:55 -0400

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import "styles/globals.css";
-import type { AppProps } from "next/app";
-
 import "aos/dist/aos.css";
 
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import AOS from "aos";
 import { useEffect } from "react";
 
@@ -10,13 +10,17 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init({
       once: true,
-      // disable: "phone",
       duration: 700,
       easing: "ease-out-cubic",
     });
   });
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
