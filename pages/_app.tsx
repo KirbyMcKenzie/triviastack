50d368b7516f40a965e26b758b4df4c0422d2bd7
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-05-29 15:49:54 -0400

    Add vercel speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "styles/globals.css";
 import "aos/dist/aos.css";
 
-import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
 import AOS from "aos";
 import { useEffect } from "react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -18,6 +19,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      <SpeedInsights />
       <Analytics />
     </>
   );
