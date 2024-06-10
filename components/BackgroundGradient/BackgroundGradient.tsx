08a82ea46ffee621b0056190388e98f2af90005f
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-06-10 16:34:09 -0400

    Fix warnings from BackgroundGradient having snake-case props

--- components/BackgroundGradient/BackgroundGradient.tsx
+++ components/BackgroundGradient/BackgroundGradient.tsx
@@ -8,26 +8,26 @@ const BackgroundGradient = () => (
   >
     <g filter="url(#filter0_f_487_2)">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1343.32 339.196C1590.7 412.639 1717.88 597.092 1675.13 741.112C1629.93 893.345 1431.16 1001.77 1169.66 924.141C882.872 839 698.31 632.794 747.876 465.835C795.229 306.329 1069.33 257.856 1343.32 339.196Z"
         fill="url(#paint0_linear_487_2)"
-        fill-opacity="0.5"
+        fillOpacity="0.5"
       />
     </g>
     <g filter="url(#filter1_f_487_2)">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M307.898 915.241C534.227 982.433 650.58 1151.19 611.463 1282.95C570.116 1422.22 388.26 1521.42 149.024 1450.4C-113.354 1372.5 -282.206 1183.85 -236.859 1031.1C-193.537 885.172 57.2319 840.825 307.898 915.241Z"
         fill="url(#paint1_linear_487_2)"
-        fill-opacity="0.5"
+        fillOpacity="0.5"
       />
     </g>
     <g filter="url(#filter2_f_487_2)">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M867.975 588.903C1140.36 669.768 1280.45 872.676 1233.43 1031.05C1183.73 1198.46 964.931 1317.63 677.01 1232.16C361.237 1138.41 157.966 911.557 212.472 727.955C264.546 552.55 566.298 499.343 867.975 588.903Z"
         fill="url(#paint2_linear_487_2)"
       />
@@ -40,9 +40,9 @@ const BackgroundGradient = () => (
         width="1543.45"
         height="1249.54"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feBlend
           mode="normal"
           in="SourceGraphic"
@@ -61,9 +61,9 @@ const BackgroundGradient = () => (
         width="1463.15"
         height="1194.25"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feBlend
           mode="normal"
           in="SourceGraphic"
@@ -82,9 +82,9 @@ const BackgroundGradient = () => (
         width="1638.76"
         height="1314.45"
         filterUnits="userSpaceOnUse"
-        color-interpolation-filters="sRGB"
+        colorInterpolationFilters="sRGB"
       >
-        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
         <feBlend
           mode="normal"
           in="SourceGraphic"
@@ -104,8 +104,8 @@ const BackgroundGradient = () => (
         y2="909.583"
         gradientUnits="userSpaceOnUse"
       >
-        <stop stop-color="#E85526" />
-        <stop offset="1" stop-color="#F6CE3E" />
+        <stop stopColor="#E85526" />
+        <stop offset="1" stopColor="#F6CE3E" />
       </linearGradient>
       <linearGradient
         id="paint1_linear_487_2"
@@ -115,8 +115,8 @@ const BackgroundGradient = () => (
         y2="1437.08"
         gradientUnits="userSpaceOnUse"
       >
-        <stop stop-color="#E85526" />
-        <stop offset="1" stop-color="#5D7BD1" />
+        <stop stopColor="#E85526" />
+        <stop offset="1" stopColor="#5D7BD1" />
       </linearGradient>
       <linearGradient
         id="paint2_linear_487_2"
@@ -126,8 +126,8 @@ const BackgroundGradient = () => (
         y2="1216.13"
         gradientUnits="userSpaceOnUse"
       >
-        <stop stop-color="#2658E8" />
-        <stop offset="1" stop-color="#5D7BD1" />
+        <stop stopColor="#2658E8" />
+        <stop offset="1" stopColor="#5D7BD1" />
       </linearGradient>
     </defs>
   </svg>
