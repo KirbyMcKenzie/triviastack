3f5e695afe4a896ec8788812efe76547f7d11d39
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-06-10 11:03:12 -0400

    Move background graident to separate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import HeroHome from "components/HeroHome/HeroHome";
 import type { NextPage } from "next";
 import NextHead from "next/head";
 import Image from "next/image";
+import BackgroundGradient from "components/BackgroundGradient";
 
 const Head = () => (
   <NextHead>
@@ -192,138 +193,7 @@ const Home: NextPage = () => {
         data-aos="zoom-y-out"
         data-aos-delay="800"
       >
-        <svg
-          width="100%"
-          height="1774"
-          viewBox="0 0 1511 1774"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g filter="url(#filter0_f_487_2)">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M1343.32 339.196C1590.7 412.639 1717.88 597.092 1675.13 741.112C1629.93 893.345 1431.16 1001.77 1169.66 924.141C882.872 839 698.31 632.794 747.876 465.835C795.229 306.329 1069.33 257.856 1343.32 339.196Z"
-              fill="url(#paint0_linear_487_2)"
-              fill-opacity="0.5"
-            />
-          </g>
-          <g filter="url(#filter1_f_487_2)">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M307.898 915.241C534.227 982.433 650.58 1151.19 611.463 1282.95C570.116 1422.22 388.26 1521.42 149.024 1450.4C-113.354 1372.5 -282.206 1183.85 -236.859 1031.1C-193.537 885.172 57.2319 840.825 307.898 915.241Z"
-              fill="url(#paint1_linear_487_2)"
-              fill-opacity="0.5"
-            />
-          </g>
-          <g filter="url(#filter2_f_487_2)">
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M867.975 588.903C1140.36 669.768 1280.45 872.676 1233.43 1031.05C1183.73 1198.46 964.931 1317.63 677.01 1232.16C361.237 1138.41 157.966 911.557 212.472 727.955C264.546 552.55 566.298 499.343 867.975 588.903Z"
-              fill="url(#paint2_linear_487_2)"
-            />
-          </g>
-          <defs>
-            <filter
-              id="filter0_f_487_2"
-              x="439.842"
-              y="0.372742"
-              width="1543.45"
-              height="1249.54"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="150"
-                result="effect1_foregroundBlur_487_2"
-              />
-            </filter>
-            <filter
-              id="filter1_f_487_2"
-              x="-544.209"
-              y="579.723"
-              width="1463.15"
-              height="1194.25"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="150"
-                result="effect1_foregroundBlur_487_2"
-              />
-            </filter>
-            <filter
-              id="filter2_f_487_2"
-              x="-96.3516"
-              y="246.109"
-              width="1638.76"
-              height="1314.45"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="150"
-                result="effect1_foregroundBlur_487_2"
-              />
-            </filter>
-            <linearGradient
-              id="paint0_linear_487_2"
-              x1="1294.28"
-              y1="324.638"
-              x2="1120.62"
-              y2="909.583"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#E85526" />
-              <stop offset="1" stop-color="#F6CE3E" />
-            </linearGradient>
-            <linearGradient
-              id="paint1_linear_487_2"
-              x1="263.035"
-              y1="901.923"
-              x2="104.161"
-              y2="1437.08"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#E85526" />
-              <stop offset="1" stop-color="#5D7BD1" />
-            </linearGradient>
-            <linearGradient
-              id="paint2_linear_487_2"
-              x1="813.982"
-              y1="572.874"
-              x2="623.017"
-              y2="1216.13"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stop-color="#2658E8" />
-              <stop offset="1" stop-color="#5D7BD1" />
-            </linearGradient>
-          </defs>
-        </svg>
+        <BackgroundGradient />
       </div>
     </>
   );
