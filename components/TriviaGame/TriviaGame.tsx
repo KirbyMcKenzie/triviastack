1b0c84ea70b64735898e5e64f6bf85e41cb94a31
Kirby McKenzie <kirby.mckenzie@hotmail.com>
2024-06-11 10:04:51 -0400

    Move Slack logo to it's own component

--- components/TriviaGame/TriviaGame.tsx
+++ components/TriviaGame/TriviaGame.tsx
@@ -5,6 +5,7 @@ import classNames from "classnames";
 const lato = Lato({ weight: ["400", "700", "900"], subsets: ["latin"] });
 import { useReward } from "react-rewards";
 import questionsData from "./questions.json";
+import LogoSlack from "components/LogoSlack";
 
 const getRandomQuestions = (questions: any, numQuestions: number = 5): any => {
   const shuffled = questions.sort(() => 0.5 - Math.random());
@@ -124,24 +125,7 @@ const TriviaGame = () => {
       <div className=" bg-white border-2 border-gray-100 max-w-[820px] mx-auto shadow-3xl text-left px-5 py-4 md:px-10 md:pt-8 md:pb-10 md:rounded-3xl duration-300 ease-in-out transition-all">
         <div className="flex items-center">
           <div className="bg-gray-50 border border-gray-200 p-1.5 md:p-2 rounded-lg h-10 w-10  md:h-14 md:w-14 hover:scale-75 transition-all duration-300">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 122.8 122.8">
-              <path
-                d="M25.8 77.6c0 7.1-5.8 12.9-12.9 12.9S0 84.7 0 77.6s5.8-12.9 12.9-12.9h12.9v12.9zm6.5 0c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9v32.3c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V77.6z"
-                fill={"#e01e5a"}
-              ></path>
-              <path
-                d="M45.2 25.8c-7.1 0-12.9-5.8-12.9-12.9S38.1 0 45.2 0s12.9 5.8 12.9 12.9v12.9H45.2zm0 6.5c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H12.9C5.8 58.1 0 52.3 0 45.2s5.8-12.9 12.9-12.9h32.3z"
-                fill={"#36c5f0"}
-              ></path>
-              <path
-                d="M97 45.2c0-7.1 5.8-12.9 12.9-12.9s12.9 5.8 12.9 12.9-5.8 12.9-12.9 12.9H97V45.2zm-6.5 0c0 7.1-5.8 12.9-12.9 12.9s-12.9-5.8-12.9-12.9V12.9C64.7 5.8 70.5 0 77.6 0s12.9 5.8 12.9 12.9v32.3z"
-                fill={"#2eb67d"}
-              ></path>
-              <path
-                d="M77.6 97c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9-12.9-5.8-12.9-12.9V97h12.9zm0-6.5c-7.1 0-12.9-5.8-12.9-12.9s5.8-12.9 12.9-12.9h32.3c7.1 0 12.9 5.8 12.9 12.9s-5.8 12.9-12.9 12.9H77.6z"
-                fill={"#ecb22e"}
-              ></path>
-            </svg>
+            <LogoSlack />
           </div>
 
           <h2 className="text-2xl md:text-4xl font-black ml-3">Play Trivia</h2>
